2dd7b3237a12323df07713762079b0c015c571b9
clap-0 <clap-0@naver.com>
2024-07-15 15:53:35 +0900

    initial commit

---
+++ src/main/java/com/likelion/deploy/LikelionDeployApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionDeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikelionDeployApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/likelion/deploy/LikelionDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikelionDeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
